3c9fa56a42bf23b24a977047fbe338634baddcba
ChrisKlp <71100241+ChrisKlp@users.noreply.github.com>
2024-04-30 19:23:25 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    })
 
-export default nextConfig;
+    return config
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+}
+
+export default nextConfig
